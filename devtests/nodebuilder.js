cc49a833f16b7dd00aacd38e68ff2e81f4bf966f
Jack Park <jackpark@gmail.com>
2014-07-06 07:41:48 -0700

    First git and some removals

---
+++ devtests/nodebuilder.js
@@ -0,0 +1,63 @@
+/**
+ * Build a testsuite of nodes
+ */
+var index = require('../index')
+  , constants = require('../lib/constants');
+
+new index(function(err,data) {
+  var Environment = data;
+  console.log('NodeBuilder-1 '+err+" "+Environment.hello());
+  var dataProvider = Environment.getDataProvider();
+	
+  var topicModel = Environment.getTopicModel();
+  //some constants
+  var lox1 = "NodeBuilderFirstTopic";
+  var lox2 = "NodeBuilderSecondTopic";
+  var lox3 = "NodeBuilderThirdTopic";
+  var lox4 = "NodeBuilderFourthTopic";
+  var lox5 = "ASuperClass";
+  var type1 = "NodeBuilderType";
+  var type2 = "NodeBuilderOtherType";
+  var proxyA, proxyB, proxyC, proxyD;
+  var credentials = null; //todo
+  //build first topic
+  topicModel.newNode(lox1,"My very first node", "we always knew this would happen", "en",
+			"SystemUser","","",false, function(data) {
+    proxyA = data;
+    Environment.logDebug('A '+proxyA.toJSON()); //TODO logging doesn't work
+    dataProvider.putNode(proxyA, function(err, data) {
+      console.log("A "+err+" "+data);
+      //second proxy
+      topicModel.newInstanceNode(lox2,type1,"First instance node","Seems likely", "en",
+  			"SystemUser","","",false, credentials, function(err,data) {
+        proxyB = data;
+        proxyB.addSuperClassLocator(lox5); //NOTE, this doesn't really deal with transitive closure
+        console.log("AB "+proxyB.toJSON());
+        dataProvider.putNode(proxyB, function(err, data) {
+            console.log("B "+err+" "+data);
+            //third proxy
+            topicModel.newInstanceNode(lox3,type1,"Second instance node","Seems likely too", "en",
+        			"SystemUser","","",false, credentials, function(err,data) {
+              proxyC = data;
+              proxyC.addSuperClassLocator(lox5); //NOTE, this doesn't really deal with transitive closure
+              dataProvider.putNode(proxyC, function(err, data) {
+                  console.log("C "+err+" "+data);
+                  //fourth proxy
+                  topicModel.newInstanceNode(lox4,type2,"Third instance node","Also Seems likely", "en",
+              			"SystemUser","","",false, credentials, function(err,data) {
+                    proxyD = data;
+                    dataProvider.putNode(proxyD, function(err, data) {
+                        console.log("D "+err+" "+data);
+               		 
+                    });
+                  });
+         		 
+              });
+            });
+  		 
+        });
+      });
+			
+    });
+  });
+});
---
+++ devtests/nodetests.js
@@ -0,0 +1,68 @@
+/**Node tests
+ */
+var index = require('../index')
+  , constants = require('../lib/constants');
+
+new index(function(err,data) {
+  var Environment = data;
+  console.log('NodeBuilder-1 '+err+" "+Environment.hello());
+  var dataProvider = Environment.getDataProvider();
+	  //some constants
+  var lox1 = "NodeBuilderFirstTopic";
+  var lox2 = "NodeBuilderSecondTopic";
+  var lox3 = "NodeBuilderThirdTopic";
+  var lox4 = "NodeBuilderFourthTopic";
+  var lox5 = "ASuperClass";
+  var type1 = "NodeBuilderType";
+  var type2 = "NodeBuilderOtherType";
+  var proxyA, proxyB, proxyC, proxyD;
+  var credentials = null; //todo
+  
+  //go looking
+  dataProvider.getNodeByLocator(lox1, credentials, function(err,data) {
+	  console.log("A "+err+" "+data.toJSON());
+	  dataProvider.getNodeByLocator(lox2, credentials, function(err,data) {
+		  console.log("B "+err+" "+data.toJSON());
+		  dataProvider.getNodeByLocator(lox3, credentials, function(err,data) {
+			  console.log("C "+err+" "+data.toJSON());
+			  dataProvider.getNodeByLocator(lox4, credentials, function(err,data) {
+				  console.log("D "+err+" "+data.toJSON());
+				  //OK to here
+				  dataProvider.listInstanceNodes(type1,0,-1, credentials, function(err,data) {
+					  console.log("E "+err+" "+data);
+					  var len = data.length;
+					  for (var i=0;i<len;i++)
+						  console.log(data[i].toJSON());
+					  dataProvider.listInstanceNodes(type2,0,-1, credentials, function(err,data) {
+						  console.log("F "+err+" "+data);
+						  var len = data.length;
+						  for (var i=0;i<len;i++)
+							  console.log(data[i].toJSON());
+						  dataProvider.listNodesByLabel("\"First instance node\"",constants.ENGLISH, 0,-1, credentials, function(err,data) {
+							  console.log("G "+err+" "+data);
+//{"locator":"NodeBuilderSecondTopic","creatorId":"SystemUser","smallIcon":"","lar
+//	geIcon":"","createdDate":"2014-12-05 14:07:41","lastEditDate":"2014-12-05 14:07:
+//		41","isPrivate":"false","label":["First instance node"],"details":["Seems likely
+//		"],"instanceOf":"NodeBuilderType","transitiveClosure":["NodeBuilderType"]}
+							  if (data) {
+							    var len = data.length;
+							    for (var i=0;i<len;i++)
+								  console.log(data[i].toJSON());
+							  }
+							  dataProvider.listSubclassNodes(lox5, 0,-1, credentials, function(err,data) {
+								  console.log("H "+err+" "+data);
+								  if (data) {
+								    var len = data.length;
+								    for (var i=0;i<len;i++)
+									  console.log(data[i].toJSON());
+								  }
+							  });
+						  });
+					  });
+					 });
+				  
+			  });
+		  });
+	  });
+  });
+});
